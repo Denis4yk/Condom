33acfc180712131af3c8f10ad9ad8470e280f35b
Bohdan Klochko <bohdanklochko1@gmail.com>
2019-03-08 12:16:45 +0200

    change file structure

---
+++ readme.md
@@ -0,0 +1,33 @@
+# Condom
+
+Protect your production app with catching undesirable input.
+
+## Installation
+
+Via Composer
+
+``` bash
+composer require obotoz/condom
+```
+
+## Usage
+
+Publish configs with:
+
+``` bash
+php artisan vendor:publish --provider="Obotoz\Condom\CondomServiceProvider" --tag=config
+```
+
+Enjoy your safety 😎 
+
+## Security
+
+If you discover any security related issues, please email riabiy.denis@gmail.com instead of using the issue tracker.
+
+## Credits
+
+- Denis Riabiy
+
+## License
+
+Nah...who reads this.
---
+++ src/CondomServiceProvider.php
@@ -0,0 +1,36 @@
+<?php
+declare(strict_types=1);
+
+namespace Obotoz\Condom;
+
+use Illuminate\Console\Events\CommandStarting;
+use Illuminate\Support\Facades\Event;
+use Illuminate\Support\ServiceProvider;
+use Obotoz\Condom\Listeners\CommandStartingListener;
+
+class CondomServiceProvider extends ServiceProvider
+{
+    /**
+     * Perform post-registration booting of services.
+     *
+     * @return void
+     */
+    public function boot(): void
+    {
+        Event::listen(CommandStarting::class, CommandStartingListener::class);
+
+        $this->publishes([
+            __DIR__ . '/../config/condom.php' => config_path('condom.php'),
+        ], 'config');
+    }
+
+    /**
+     * Register any package services.
+     *
+     * @return void
+     */
+    public function register(): void
+    {
+        $this->mergeConfigFrom(__DIR__ . '/../config/condom.php', 'condom');
+    }
+}
